fe273904a481b0083e638aa629f987d39b714530
doctork4 <doctor.k1994@gmail.com>
2023-07-17 16:53:28 +0900

    Chore:: commitlint.config.js 설정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'subject-empty': [2, 'always'],
+    'type-empty': [2, 'always'],
     'type-enum': [
       2,
       'always',
-      ['Feat::', 'Update::', 'Fix::', '!BREAKING CHANGE::', '!HOTFIX::', 'Docs::', 'Style::', 'Refactor::', 'Test::', 'Rename::', 'Chore::', 'Comment::', 'Remove::'],
+      [
+        'Feat::',
+        'Update::',
+        'Fix::',
+        '!BREAKING CHANGE::',
+        '!HOTFIX::',
+        'Docs::',
+        'Style::',
+        'Refactor::',
+        'Test::',
+        'Rename::',
+        'Chore::',
+        'Comment::',
+        'Remove::',
+      ],
     ],
   },
 };
