257201dcb59caea5532eac122e820a8ea6578e36
Dongryul KIM <68728192+DoctorK4@users.noreply.github.com>
2023-07-24 10:39:07 +0900

    Develop (#3) 환경변수 설정 (eslint, prettier, husky, storybook, lint-staged, tsconfig)
    * Chore:: prettier, eslint 설정
    
    * Test:: husky commitlint 테스트
    
    * Chore:: commitlint.config.js 설정
    
    * Chore:: Storybook 설치
    
    * Chore:: 스토리북 예시 컴포넌트 추가
    
    * Chore:: .eslintcache gitignore 에 추가
    
    * Chore:: eslint jsx 확장자 유연성 규칙 추가
    
    * Chore:: push를 위한 formatting & lint ignore 선언
    
    * Test:: lint staged test
    
    * Test:: formatting test
    
    * Test:: lint-staged test
    
    * Test:: Husky 명령어 test
    
    * Test:: husky test
    
    * Test:: lint-staged test
    
    * Test:: lint-staged test 2
    
    * Test:: lint-staged test 3
    
    * Chore:: tsconfig parser 설정, prettier  중복 옵션 제거

---
+++ stories/Button.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/require-default-props */
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable react/no-unescaped-entities */
+import "./button.css";
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " ",
+      )}
+      {...props}
+    >
+      {label}
+      <style jsx>{`
+        button {
+          background-color: ${backgroundColor};
+        }
+      `}</style>
+    </button>
+  );
+};
