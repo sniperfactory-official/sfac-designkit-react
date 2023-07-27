c76ff68a4d126212fd9e703b686a0ed9877a5cc2
Dongryul KIM <68728192+DoctorK4@users.noreply.github.com>
2023-07-27 15:00:14 +0900

    Feature/common > Design System 제작을 위한 setting 공통 적용 사항 반영 (#28)
    * Feat:: Title 컴포넌트  생성 및 기본 alias 설정
    
    * Fix:: code format 적용
    
    * Feat:: Text 컴포넌트 생성
    
    * Feat:: Logo 컴포넌트 생성
    
    * Feat:: npm 배포를 위한 의존성 설치
    
    * Chore:: Format 적용
    
    * Fix:: Font-family 적용 수정
    
    * Docs:: Create pull_request_template.md
    
    * Chore:: components로 폴더명 변경
    
    * Feat:: Logo 컴포넌트 블럭처리 & className 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ReactNode } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
