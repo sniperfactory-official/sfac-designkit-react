4342a128e58cd61342d15f5447c007010e8a61a2
hayoenn2 <dwd1205@naver.com>
2023-07-31 16:47:24 +0900

    Update:: onClick 함수 대신 active 속성으로 변경

--- app/components/Tab/Tab.tsx
+++ app/components/Tab/Tab.tsx
@@ -1,30 +1,24 @@
 "use client";
 
-import { useState } from "react";
 import Text, { TextProps } from "@/app/components/Text.tsx";
 
 type ButtonProps = {
   text: string;
   textSize?: TextProps["size"];
   textWeight?: TextProps["weight"];
+  active: boolean;
 };
 export default function Tab({
   text,
   textSize = "base",
   textWeight = "semibold",
+  active = false,
 }: ButtonProps) {
-  const [activeTab, setActiveTab] = useState(false);
-
-  const isActiveTab = () => {
-    setActiveTab(prev => !prev);
-  };
-
   return (
     <button
       type="button"
-      onClick={isActiveTab}
       className={`border-b-4 transition-colors duration-300 ${
-        activeTab ? "border-Primary-100 text-Primary-100" : "border-transparent"
+        active ? "border-Primary-100 text-Primary-100" : "border-transparent"
       } w-[347px] text-center pt-[14px] pb-[17px]`}
     >
       <Text size={textSize} weight={textWeight}>
