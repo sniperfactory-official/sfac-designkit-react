53f1c84576bfa767968919b34193128278e0eb32
Jaebin <wjdwoqls777@naver.com>
2023-07-31 19:27:12 +0900

    init

---
+++ src/components/sfac-player/sfac-player.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sfac-player.css";
+
+const SFACPlayer = () => {
+  return (
+    <div className="sfac-player">
+      <div>hi dd</div>
+    </div>
+  );
+};
+
+export default SFACPlayer;
---
+++ src/sfac-player.tsx
@@ -0,0 +1,3 @@
+import SFACPlayer from "./components/sfac-player/sfac-player";
+
+export default SFACPlayer;
