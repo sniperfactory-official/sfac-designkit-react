2234df30af2fa4674a3de672c1d4cbe9afb87de7
Eunyoung Park <108717962+Zoey425@users.noreply.github.com>
2023-07-31 15:42:27 +0900

    Feat:: Breadcrumb 컴포넌트 생성 (#34)
    Co-authored-by: Dongryul KIM <68728192+DoctorK4@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-"use client";
-
 import Text from "@/app/components/Text.tsx";
 import Title from "@/app/components/Title.tsx";
 import Logo from "@/app/components/Logo.tsx";
-import Button from "./components/Button/Button.tsx";
 
 export default function Home() {
-  const onClick = () => {
-    // eslint-disable-next-line no-console
-    console.log("123");
-  };
-
   return (
     <>
       <Title size="4xl">제목</Title>
@@ -18,13 +10,6 @@ export default function Home() {
         안녕하세요
       </Text>
       <Logo width={500} height={300} className="pl-8 bg-slate-500" />
-      <Button
-        variant="primary"
-        text="로그인"
-        onClick={onClick}
-        disabled
-        asChild
-      />
     </>
   );
 }
