ba16554432808f06967a7dcdb5d6ae36d4f84fc1
Dongryul KIM <68728192+DoctorK4@users.noreply.github.com>
2023-07-31 21:59:01 +0900

    Feat:: Avatar 컴포넌트 제작 (#35)
    * Feat:: Avatar 컴포넌트 제작
    
    * Feat:: Avatar 컴포넌트 제작
    
    * Chore:: Avatar 컴포넌트 테스트 삭제

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "build",
+  images: {
+    domains: ["firebasestorage.googleapis.com", "www.gravatar.com"],
+  },
 };
 
 module.exports = nextConfig;
