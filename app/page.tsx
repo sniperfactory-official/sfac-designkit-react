bdaf9e8fcd4b9261fa11e10888f7fc16b21d1d58
doctork4 <doctor.k1994@gmail.com>
2023-08-01 23:36:16 +0900

    Fix:: Test용 page.tsx 원복

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Text from "@/app/components/Text.tsx";
 import Title from "@/app/components/Title.tsx";
 import Logo from "@/app/components/Logo.tsx";
-import Card from "@/app/components/Card/Card.tsx";
 
 export default function Home() {
   return (
@@ -11,24 +10,6 @@ export default function Home() {
         안녕하세요
       </Text>
       <Logo width={500} height={300} className="pl-8 bg-slate-500" />
-      <Card CardType="Comment" className="ml-4 mt-4">
-        가나다
-      </Card>
-      <Card CardType="Lecture" className="ml-4 mt-4 font-bold">
-        가나다
-      </Card>
-      <Card CardType="Post" className="ml-4 mt-4">
-        가나다
-      </Card>
-      <Card CardType="Vertical" className="ml-4 mt-4">
-        가나다
-      </Card>
-      <Card CardType="Photo" className="ml-4 mt-4">
-        가나다
-      </Card>
-      <Card CardType="Content" className="ml-4 mt-4">
-        가나다
-      </Card>
     </>
   );
 }
