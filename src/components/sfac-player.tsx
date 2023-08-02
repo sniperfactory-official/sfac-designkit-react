67508099a58029b333ec6def02b0ce8c6bc0ed95
omer_in <107422716+ome-r@users.noreply.github.com>
2023-08-02 17:47:19 +0900

    inhae, <feat> :: props 정의

--- src/components/sfac-player.tsx
+++ src/components/sfac-player.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './styles/sfac-player.css';
+import SFACPlayerProps from './SFACPlayerProps';
 
-interface SFACPlayerProps {
-  src: string;
-}
-
-const SFACPlayer: React.FC<SFACPlayerProps> = ({src}) => {
+const SFACPlayer: React.FC<SFACPlayerProps> = ({url}) => {
   return (
     <div>
       <video className="sfac-player" controls>
-        <source src={src} type="video/mp4" />
+        <source src={url} type="video/mp4" />
         Your browser does not support the video tag. ss
       </video>
     </div>
