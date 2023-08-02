2cf08bdff987a282ed52cc89079f5b8dbcf1c801
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-08-02 13:10:30 +0900

    Update:: Icon width,height 와 clasName 추가, icon 타입별로 폴더정리

--- app/components/Icon/Icon.tsx
+++ app/components/Icon/Icon.tsx
@@ -5,12 +5,25 @@ export type IconName = keyof typeof Icons;
 export const IconComponents = Icons;
 
 interface IconProps extends HTMLAttributes<SVGElement> {
-  size?: number;
   name: IconName;
+  width?: number;
+  height?: number;
   className?: string;
 }
 
-export default function Icon({ size = 21, name, className }: IconProps) {
-  const IconComponent = IconComponents[name];
-  return <IconComponent width={size} height={size} className={className} />;
+export default function Icon({
+  width = 21,
+  height = 21,
+  name,
+  className,
+}: IconProps) {
+  const IconComponent = Icons[name];
+
+  return (
+    <IconComponent
+      width={width}
+      height={height}
+      className={`${className} fill-Grayscale-60`}
+    />
+  );
 }
--- app/components/Icon/index.ts
+++ app/components/Icon/index.ts
@@ -1,20 +1,25 @@
-export { default as Hello } from "@/app/assets/icons/Hello.svg";
-export { default as Agree } from "@/app/assets/icons/Agree.svg";
-export { default as AI } from "@/app/assets/icons/AI.svg";
-export { default as AngelFace } from "@/app/assets/icons/AngelFace.svg";
-export { default as Arrow } from "@/app/assets/icons/Arrow.svg";
-export { default as CoverBox } from "@/app/assets/icons/CoverBox.svg";
-export { default as Data } from "@/app/assets/icons/Data.svg";
-export { default as Design } from "@/app/assets/icons/Design.svg";
-export { default as Disagree } from "@/app/assets/icons/Disagree.svg";
-export { default as Folder } from "@/app/assets/icons/Folder.svg";
-export { default as Game } from "@/app/assets/icons/Game.svg";
-export { default as Hit } from "@/app/assets/icons/Hit.svg";
-export { default as Link } from "@/app/assets/icons/Link.svg";
-export { default as Note } from "@/app/assets/icons/Note.svg";
-export { default as Quality } from "@/app/assets/icons/Quality.svg";
-export { default as Security } from "@/app/assets/icons/Security.svg";
-export { default as SkipNext } from "@/app/assets/icons/SkipNext.svg";
-export { default as Sound } from "@/app/assets/icons/Sound.svg";
-export { default as Web } from "@/app/assets/icons/Web.svg";
-export { default as WideScreen } from "@/app/assets/icons/WideScreen.svg";
+// imgIcon
+export { default as Hello } from "@/app/assets/icons/imgIcon/Hello.svg";
+export { default as Agree } from "@/app/assets/icons/imgIcon/Agree.svg";
+export { default as AI } from "@/app/assets/icons/imgIcon/AI.svg";
+export { default as All } from "@/app/assets/icons/imgIcon/All.svg";
+export { default as AngelFace } from "@/app/assets/icons/imgIcon/AngelFace.svg";
+export { default as Data } from "@/app/assets/icons/imgIcon/Data.svg";
+export { default as Design } from "@/app/assets/icons/imgIcon/Design.svg";
+export { default as Disagree } from "@/app/assets/icons/imgIcon/Disagree.svg";
+export { default as Folder } from "@/app/assets/icons/imgIcon/Folder.svg";
+export { default as Game } from "@/app/assets/icons/imgIcon/Game.svg";
+export { default as Hit } from "@/app/assets/icons/imgIcon/Hit.svg";
+export { default as Link } from "@/app/assets/icons/imgIcon/Link.svg";
+export { default as Note } from "@/app/assets/icons/imgIcon/Note.svg";
+export { default as Security } from "@/app/assets/icons/imgIcon/Security.svg";
+export { default as Sound } from "@/app/assets/icons/imgIcon/Sound.svg";
+export { default as Web } from "@/app/assets/icons/imgIcon/Web.svg";
+
+// strokeIcon
+export { default as Arrow } from "@/app/assets/icons/stroke/Arrow.svg";
+export { default as CoverBox } from "@/app/assets/icons/stroke/CoverBox.svg";
+export { default as Quality } from "@/app/assets/icons/stroke/Quality.svg";
+export { default as SkipNext } from "@/app/assets/icons/stroke/SkipNext.svg";
+export { default as WideScreen } from "@/app/assets/icons/stroke/WideScreen.svg";
+export { default as BreadArrow } from "@/app/assets/icons/stroke/BreadArrow.svg";
