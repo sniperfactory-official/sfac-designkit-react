97b6015361085ad64f091f49c9e59f3264d70812
Uichae <ghddmlco@gmail.com>
2023-08-05 17:40:36 +0900

    Update:: 코드 간소화

--- app/components/Searchfield/Searchfield.tsx
+++ app/components/Searchfield/Searchfield.tsx
@@ -5,9 +5,15 @@ import magnifyingfocus from "@/app/assets/magnifyingfocus.svg";
 
 export interface SearchfieldProps {
   onClickSearchfield?: (value: string) => void;
+  placeholder?: string;
+  className?: string;
 }
 
-export default function Searchfield({ onClickSearchfield }: SearchfieldProps) {
+export default function Searchfield({
+  onClickSearchfield,
+  placeholder = "검색어를 입력해주세요",
+  className,
+}: SearchfieldProps) {
   const [focus, setFocus] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -22,12 +28,18 @@ export default function Searchfield({ onClickSearchfield }: SearchfieldProps) {
       handleSearchfield();
     }
   };
+  const wrapperFocusStyle = focus ? "border-blue-500" : "border-Grayscale-10";
+  const placeholderText = focus ? "" : `${placeholder}`;
+
   return (
     <div>
-      {focus ? (
-        <div className="input-wrapper w-[245px] h-[40px] outline-none border border-blue-500 rounded-[10px]">
+      <div
+        className={`input-wrapper w-[245px] h-[40px] outline-none border rounded-[10px] ${wrapperFocusStyle} ${className}`}
+      >
+        <div>
           <input
             ref={inputRef}
+            placeholder={placeholderText}
             type="text"
             id="Searchfield"
             className="relative top-[7px] left-[15px] w-[182px] h-[17px] font-medium text-sm outline-none text-Grayscale-80"
@@ -35,45 +47,20 @@ export default function Searchfield({ onClickSearchfield }: SearchfieldProps) {
             onBlur={() => setFocus(false)}
             onKeyDown={handleKeyDown}
           />
-          <div
-            onClick={handleSearchfield}
-            role="button"
-            tabIndex={0}
-            onKeyPress={handleKeyDown}
-          >
-            <Image
-              src={magnifying}
-              alt="돋보기"
-              className="relative top-[-13px] left-[212px]"
-            />
-          </div>
         </div>
-      ) : (
-        <div className="input-wrapper w-[245px] h-[40px] outline-none border border-Grayscale-10 rounded-[10px]">
-          <input
-            ref={inputRef}
-            type="text"
-            id="Searchfield"
-            className="relative top-[7px] left-[15px] w-[182px] h-[17px] font-medium text-sm outline-none text-Grayscale-80"
-            placeholder="검색어를 입력해주세요"
-            onFocus={() => setFocus(true)}
-            onBlur={() => setFocus(false)}
-            onKeyDown={handleKeyDown}
+        <div
+          onClick={handleSearchfield}
+          role="button"
+          tabIndex={0}
+          onKeyPress={handleKeyDown}
+        >
+          <Image
+            src={focus ? magnifying : magnifyingfocus}
+            alt="돋보기"
+            className="relative top-[-13px] left-[212px]"
           />
-          <div
-            onClick={handleSearchfield}
-            role="button"
-            tabIndex={0}
-            onKeyPress={handleKeyDown}
-          >
-            <Image
-              src={magnifyingfocus}
-              alt="돋보기"
-              className="relative top-[-13px] left-[212px]"
-            />
-          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
