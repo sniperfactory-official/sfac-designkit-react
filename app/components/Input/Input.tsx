0d7f15eb2f008550d2e1f59c817736a1136775e1
Uichae <ghddmlco@gmail.com>
2023-08-05 16:38:56 +0900

    Update:: 코드 수정

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,81 +1,59 @@
 import React from "react";
+import Text from "../Text";
 
 export interface InputProps {
-  mode?: "Text" | "Label" | "Default";
+  type?: "text" | "password";
   onChangeInputText?: (value: string) => void;
-  onChangeInputLabel?: (value: string) => void;
-  onChangeInputDefault?: (value: string) => void;
+  className?: string;
+  placeholder?: string;
+  label?: string;
+  text?: string;
+  textColor?: "red" | "blue" | "gray";
+  labelColor?: "red" | "blue" | "gray";
 }
 
 export default function Input({
-  mode = "Default",
-  onChangeInputLabel,
+  type = "text",
+  placeholder = "이메일 주소",
+  label,
+  text,
+  className,
+  textColor = "gray",
+  labelColor = "blue",
   onChangeInputText,
-  onChangeInputDefault,
 }: InputProps) {
   const handleInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (onChangeInputText) {
       onChangeInputText(e.target.value);
     }
   };
-  const handleInputLabel = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (onChangeInputLabel) {
-      onChangeInputLabel(e.target.value);
-    }
+
+  const textColorMapping = {
+    red: "text-Meaningful-red",
+    blue: "text-Primary-80",
+    gray: "text-Grayscale-40",
   };
-  const handleInputDefault = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (onChangeInputDefault) {
-      onChangeInputDefault(e.target.value);
-    }
-  };
-  if (mode === "Text") {
-    return (
-      <div className="w-[422px] h-[87px] rounded relative top-4 left-4">
-        <span className="text-Primary-80 font-bold w-9 h-[17px] relative left-[10px]">
-          이메일
-        </span>
-        <br />
-        <input
-          type="email"
-          id="Text"
-          placeholder="이메일 주소"
-          className="bg-Grayscale-5 w-[422px] h-[48px] pr-4 pt-[10px] pl-4 pb-[10px] outline-none rounded-[10px]"
-          onChange={handleInputText}
-        />
-      </div>
-    );
-  }
-  if (mode === "Label") {
-    return (
-      <div className="w-[422px] h-[87px] rounded relative top-4 left-4">
-        <span className="text-Primary-80 font-bold w-[36px] h-[17px] relative left-[10px]">
-          이메일
-        </span>
-        <br />
-        <input
-          type="email"
-          id="Label"
-          placeholder="이메일 주소"
-          className="bg-Grayscale-5 w-[422px] h-[48px] pr-4 pt-[10px] pl-4 pb-[10px] outline-none rounded-[10px]"
-          onChange={handleInputLabel}
-        />
-        <br />
-        <span className="text-Grayscale-40 font-medium text-xs relative left-[10px]">
-          이메일을 입력해주세요
-        </span>
-      </div>
-    );
-  }
-  if (mode === "Default") {
-    return (
+
+  return (
+    <>
+      <p className="ml-4">
+        {/* <Text size="sm" weight="bold" className={`text-Primary-80 `}> */}
+        <Text size="sm" weight="bold" className={textColorMapping[labelColor]}>
+          {label}
+        </Text>
+      </p>
       <input
-        type="email"
-        id="Default"
-        placeholder="이메일 주소"
-        className="bg-Grayscale-5 w-[422px] h-[48px] pr-4 pt-[10px] pl-4 pb-[10px] outline-none rounded-[10px] relative top-4 left-4"
-        onChange={handleInputDefault}
+        type={type}
+        id="Text"
+        placeholder={placeholder}
+        className={`bg-Grayscale-5 w-[422px] h-[48px] pr-4 pt-[10px] pl-4 pb-[10px] outline-none rounded-[10px] ${className}`}
+        onChange={handleInputText}
       />
-    );
-  }
-  return null;
+      <p className="ml-4">
+        <Text size="xs" weight="medium" className={textColorMapping[textColor]}>
+          {text}
+        </Text>
+      </p>
+    </>
+  );
 }
