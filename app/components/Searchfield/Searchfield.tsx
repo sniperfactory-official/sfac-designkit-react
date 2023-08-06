bf5cae6f851bd740209baac1f2e560ace5e84488
UichaeHong <127120819+UichaeHong@users.noreply.github.com>
2023-08-06 23:22:09 +0900

    Feat:: Searchfield component 생성 (#42)
    * Feat:: Searchfield component 생성
    
    * Update:: Image component 추가, 이벤트핸들러 수정 및 추가
    
    * Update:: 이벤트핸들러 수정
    
    * Update:: 코드 간소화
    
    * Update:: props name 수정
    
    * Fix:: Searchfield 컴포넌트 수정
    
    ---------
    
    Co-authored-by: hayoenn2 <dwd1205@naver.com>
    Co-authored-by: HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>

---
+++ app/components/Searchfield/Searchfield.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import React, { useState, useRef } from "react";
+import magnifyingfocus from "@/app/assets/magnifyingfocus.svg";
+import magnifying from "@/app/assets/magnifying.svg";
+
+export interface SearchfieldProps {
+  onClick?: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export default function Searchfield({
+  onClick,
+  placeholder = "검색어를 입력해주세요",
+  className,
+}: SearchfieldProps) {
+  const [focus, setFocus] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSearchfield = () => {
+    if (onClick && inputRef.current) {
+      onClick(inputRef.current.value);
+      inputRef.current.value = "";
+    }
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearchfield();
+    }
+  };
+  const wrapperFocusStyle = focus ? "border-blue-500" : "border-Grayscale-10";
+  const placeholderText = focus ? "" : `${placeholder}`;
+
+  return (
+    <div>
+      <div
+        className={`${wrapperFocusStyle} ${className} px-[15px] relative border w-[245px] py-3 rounded-[10px] flex`}
+      >
+        <input
+          ref={inputRef}
+          placeholder={placeholderText}
+          type="text"
+          id="Searchfield"
+          className=" font-medium text-sm text-Grayscale-80 w-[182px]"
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          onKeyDown={handleKeyDown}
+        />
+        <div className="flex right-[15px] absolute">
+          <button
+            type="button"
+            className="w-[20px] h-[20px] text-[0]"
+            onClick={handleSearchfield}
+          >
+            돋보기
+            <Image src={focus ? magnifying : magnifyingfocus} alt="돋보기" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
