3e79719c566eaf905ee4d0e200dfee529c738485
Uichae <ghddmlco@gmail.com>
2023-08-07 00:37:17 +0900

    Update:: 작성자 수정

--- app/components/textarea/Textarea.tsx
+++ app/components/textarea/Textarea.tsx
@@ -1,13 +1,15 @@
 import React, { TextareaHTMLAttributes } from "react";
+import Text from "../Text";
 
 export interface TextareaProps
   extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   mode: "title" | "content";
   onChangeTitle?: (value: string) => void;
-  onChangeAuthor?: (value: string) => void;
+  // onChangeAuthor?: (value: string) => void;
   onChangeContent?: (value: string) => void;
   className?: string;
   placeholder?: string;
+  author?: string;
 }
 
 export default function Textarea({
@@ -16,19 +18,14 @@ export default function Textarea({
   className,
   placeholder = "입력해주세요",
   onChangeTitle,
-  onChangeAuthor,
+  // onChangeAuthor,
+  author,
   onChangeContent,
   ...textareaProps
 }: TextareaProps) {
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (onChangeTitle) {
       onChangeTitle(e.target.value);
-    }
-  };
-
-  const handleAuthorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (onChangeAuthor) {
-      onChangeAuthor(e.target.value);
     }
   };
 
@@ -43,25 +40,29 @@ export default function Textarea({
   if (mode === "title") {
     return (
       <div className={`h-[79px] border border-[#E8E8E8] ${Style} ${className}`}>
-        <div className="w-[362px] relative top-[19px] left-[14px]">
+        <div className="w-[362px]">
           <p>
             <input
               id="titleInput"
               placeholder={placeholder}
               type="text"
-              className="outline-none"
+              className="outline-none ml-[14px] mt-[19px] w-[362px]"
               onChange={handleTitleChange}
             />
           </p>
           <p>
-            <label htmlFor="authorInput" className="text-Grayscale-40 text-xs">
-              작성자&nbsp;
-              <input
-                id="authorInput"
-                type="text"
-                className="outline-none"
-                onChange={handleAuthorChange}
-              />
+            <label
+              htmlFor="myTitle"
+              className="text-Grayscale-40 text-xs ml-[14px]"
+            >
+              작성자
+              <Text
+                size="xs"
+                weight="bold"
+                className="text-Grayscale-60 ml-[6px]"
+              >
+                {author}
+              </Text>
             </label>
           </p>
         </div>
@@ -70,15 +71,16 @@ export default function Textarea({
   }
 
   if (mode === "content") {
-    const textareaClass = `border p-2 h-[234px] outline-none ${Style} ${className} ${
+    const textareaClass = `border h-[234px] outline-none ${Style} ${className} ${
       disabled ? "Grayscale-0 text-Grayscale-20" : ""
     }`;
 
     return (
-      <div>
+      <div className={textareaClass}>
         <textarea
+          id="myTextarea"
           placeholder={placeholder}
-          className={textareaClass}
+          className="ml-[22px] mt-[22px] w-[535px] h-[190px] outline-none resize-none"
           disabled={disabled}
           onChange={handleTextareaChange}
           rows={textareaProps.rows}
