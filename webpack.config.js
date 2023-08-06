675b03d70688a106728e2e24da925c3800b5f069
hayoenn2 <dwd1205@naver.com>
2023-08-06 15:08:54 +0900

    Test:: webpack 수정 - nextjs에서 build 후 production mode에서 tailwind가 적용되지 않는 문제 해결

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
+  mode: "production",
+  entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
     umdNamedDefine: true,
-    globalObject: 'this',
+    globalObject: "this",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'ts-loader',
+        use: "ts-loader",
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader', 'postcss-loader' ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
   },
   externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
+    react: "react",
+    "react-dom": "react-dom",
   },
+  plugins: [new MiniCssExtractPlugin({ filename: "style.css" })],
 };
