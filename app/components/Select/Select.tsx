7d0f2140b13e67fa6a963743771278be905cc762
Uichae <ghddmlco@gmail.com>
2023-08-06 17:39:44 +0900

    Feat:: 수정

--- app/components/Select/Select.tsx
+++ app/components/Select/Select.tsx
@@ -1,153 +1,74 @@
-// import React, { useState, ReactNode } from "react";
-
-// type SelectProps = {
-//   mode: "Main" | "Sub";
-//   children?: ReactNode;
-// };
-
-// export default function Select({ mode, children }: SelectProps) {
-//   const [focused, setFocused] = useState(false);
-
-//   return (
-//     <div>
-//       {focused ? (
-//         <div
-//           className={`w-[245px] h-[46px] outline-none rounded-[10px] relative top-3 left-5 ${
-//             mode === "Main" ? "bg-blue-100" : ""
-//           }`}
-//           onFocus={() => setFocused(true)}
-//           onBlur={() => setFocused(false)}
-//           tabIndex={0}
-//         >
-//           <div
-//             className={`relative top-[13px] left-[55px] text-Grayscale-80 ${
-//               mode === "Sub" ? "text-Primary-80" : ""
-//             }`}
-//           >
-//             {children}
-//           </div>
-//         </div>
-//       ) : (
-//         <div
-//           className={`w-[245px] h-[46px] outline-none rounded-[10px] relative top-3 left-5`}
-//           onFocus={() => setFocused(true)}
-//           onBlur={() => setFocused(false)}
-//           tabIndex={0}
-//         >
-//           <div className="relative top-[13px] left-[55px] text-Grayscale-80">
-//             {children}
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-// "use client";
-// import React, { useState, ReactNode } from "react";
-// import Image from "next/image";
-
-// type SelectProps = {
-//   mode: "Main" | "Sub";
-//   children?: ReactNode;
-//   img?: string; // 추가된 prop
-//   className?: string;
-// };
-
-// export default function Select({
-//   mode,
-//   children,
-//   img,
-//   className,
-// }: SelectProps) {
-//   const [focused, setFocused] = useState(false);
-
-//   const Style =
-//     "w-[245px] h-[46px] outline-none rounded-[10px] relative top-3 left-5";
-
-//   return (
-//     <div>
-//       {focused ? (
-//         <div
-//           className={`${Style} ${
-//             mode === "Main" ? "bg-blue-100" : ""
-//           } ${className}`}
-//           onFocus={() => setFocused(true)}
-//           onBlur={() => setFocused(false)}
-//           tabIndex={0}
-//         >
-//           <div
-//             className={`relative top-[13px] left-[55px] text-Grayscale-80 ${
-//               mode === "Sub" ? "text-Primary-80" : ""
-//             }`}
-//           >
-//             {children}
-//           </div>
-//           {img && (
-//             <div className="absolute top-[10px] left-[10px]">
-//               <Image src={img} alt="Icon" width={20} height={20} />
-//             </div>
-//           )}
-//         </div>
-//       ) : (
-//         <div
-//           className={`${Style} ${className}`}
-//           onFocus={() => setFocused(true)}
-//           onBlur={() => setFocused(false)}
-//           tabIndex={0}
-//         >
-//           <div className="relative top-[13px] left-[55px] text-Grayscale-80">
-//             {children}
-//           </div>
-//           {img && (
-//             <div className="absolute top-[10px] left-[10px]">
-//               <Image src={img} alt="Icon" width={20} height={20} />
-//             </div>
-//           )}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-"use client";
-
 import React, { useState, ReactNode } from "react";
+import Arrow from "@/app/assets/arrow.svg";
+// import toggleButton from "@/app/assets/toggleButton.svg";
+import Image from "next/image";
 
 type SelectProps = {
   mode: "Main" | "Sub";
   children?: ReactNode;
+  className?: string;
+  focusedClassName?: string;
+  src?: string;
 };
 
-export default function Select({ mode, children }: SelectProps) {
+export default function Select({
+  mode,
+  children,
+  className,
+  focusedClassName,
+  src = Arrow,
+}: SelectProps) {
   const [focused, setFocused] = useState(false);
+
+  const divStyle = "text-Grayscale-80 flex items-center justify-center";
+  const buttonStyle =
+    "w-[245px] h-[46px] outline-none border-none rounded-[10px] relative top-3 left-5";
 
   return (
     <div>
       {focused ? (
         <button
           type="button"
-          className={`w-[245px] h-[46px] outline-none border-none rounded-[10px] relative top-3 left-5 ${
+          className={`${buttonStyle} ${className} ${focusedClassName} ${
             mode === "Main" ? "bg-blue-100" : ""
           }`}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
         >
           <div
-            className={`relative top-[13px] left-[55px] text-Grayscale-80 ${
+            className={`${divStyle} ${focusedClassName} ${
               mode === "Sub" ? "text-Primary-80" : ""
             }`}
           >
+            {mode === "Main" && (
+              <Image
+                src={src}
+                alt="Icon"
+                width={20}
+                height={20}
+                // className="ml-6 mr-6"
+              />
+            )}
             {children}
           </div>
         </button>
       ) : (
         <button
           type="button"
-          className="w-[245px] h-[46px] outline-none border-none rounded-[10px] relative top-3 left-5"
+          className={`${buttonStyle} ${className}`}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
         >
-          <div className="relative top-[13px] left-[55px] text-Grayscale-80">
+          <div className={divStyle}>
+            {mode === "Main" && (
+              <Image
+                src={src}
+                alt="Icon"
+                width={20}
+                height={20}
+                // className="ml-6 mr-6"
+              />
+            )}
             {children}
           </div>
         </button>
@@ -155,3 +76,67 @@ export default function Select({ mode, children }: SelectProps) {
     </div>
   );
 }
+
+// import React, { useState, ReactNode } from "react";
+// import Arrow from "@/app/assets/arrow.svg";
+// import toggleButton from "@/app/assets/toggleButton.svg";
+// import Image from "next/image";
+
+// type SelectProps = {
+//   mode: "Main" | "Sub";
+//   children?: ReactNode;
+//   className?: string;
+//   focusedClassName?: string;
+//   src?: React.ComponentType<{ className?: string }>;
+// };
+
+// export default function Select({
+//   mode,
+//   children,
+//   className,
+//   focusedClassName,
+//   src = Arrow, // 기본값은 Arrow로 설정합니다.
+// }: SelectProps) {
+//   const [focused, setFocused] = useState(false);
+//   const SvgIcon = src;
+
+//   const divStyle = "text-Grayscale-80 flex items-center justify-center";
+//   const buttonStyle =
+//     "w-[245px] h-[46px] outline-none border-none rounded-[10px] relative top-3 left-5";
+
+//   return (
+//     <div>
+//       {focused ? (
+//         <button
+//           type="button"
+//           className={`${buttonStyle} ${className} ${focusedClassName} ${
+//             mode === "Main" ? "bg-blue-100" : ""
+//           }`}
+//           onFocus={() => setFocused(true)}
+//           onBlur={() => setFocused(false)}
+//         >
+//           <div
+//             className={`${divStyle} ${focusedClassName} ${
+//               mode === "Sub" ? "text-Primary-80" : ""
+//             }`}
+//           >
+//             {mode === "Main" && <SvgIcon className="h-5 w-5" />}
+//             {children}
+//           </div>
+//         </button>
+//       ) : (
+//         <button
+//           type="button"
+//           className={`${buttonStyle} ${className}`}
+//           onFocus={() => setFocused(true)}
+//           onBlur={() => setFocused(false)}
+//         >
+//           <div className={`${divStyle}`}>
+//             {mode === "Main" && <SvgIcon className="h-5 w-5" />}
+//             {children}
+//           </div>
+//         </button>
+//       )}
+//     </div>
+//   );
+// }
