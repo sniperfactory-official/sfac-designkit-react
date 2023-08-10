af49223326717162c0152c4ce0132e4efd4c849a
hayoenn2 <dwd1205@naver.com>
2023-08-10 16:46:42 +0900

    Refacor:: Toast 컴포넌트 style 수정

--- app/components/Toast/Toast.tsx
+++ app/components/Toast/Toast.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { twMerge } from "tailwind-merge";
 import Text, { TextProps } from "@/app/components/Text.tsx";
 import Title, { TitleProps } from "@/app/components/Title.tsx";
 import Button from "@/app/components/Button/Button";
@@ -26,6 +28,7 @@ export default function Toast({
   let toastStyle = "border-Grayscale-10 text-Grayscale-60";
   let actionButton = "";
   let titleStyle = "";
+  const textStyle = "";
 
   switch (type) {
     case "Success":
@@ -35,7 +38,7 @@ export default function Toast({
       toastStyle = "text-Meaningful-red bg-[#FCF5F5] border-Meaningful-red";
       break;
     case "With Action":
-      actionButton = "text-Primary-40 absolute right-5 top-[20px]";
+      actionButton = "text-Primary-40 w-[88px]";
       titleStyle = "block text-Grayscale-80";
       break;
     case "With Title":
@@ -46,26 +49,37 @@ export default function Toast({
   }
   return (
     <div
-      className={`relative w-[360px] break-normal shadow-toast rounded-[10px] border px-5 py-4 ${toastStyle} ${className}`}
+      className={twMerge(
+        `w-[360px] flex break-all shadow-toast rounded-[10px] border px-5 py-4 tracking-[-0.02rem] ${toastStyle}`,
+        className,
+      )}
     >
-      {title && (
-        <Title size={titleSize} className={titleStyle}>
-          {title}
-        </Title>
-      )}
-      <Text size={textSize} weight={textWeight}>
-        {text}
-      </Text>
-      {button && (
-        <Button
-          text="확인"
-          variant="outline"
-          asChild
-          className={actionButton}
-          textSize="sm"
-          textWeight="semibold"
-        />
-      )}
+      <div className="w-auto">
+        {title && (
+          <Title size={titleSize} className={titleStyle}>
+            {title}
+          </Title>
+        )}
+        <Text size={textSize} weight={textWeight} className={textStyle}>
+          {text}
+        </Text>
+      </div>
+      <div
+        className={`${
+          type === "With Action" ? "ml-[35px]" : ""
+        } flex items-center`}
+      >
+        {button && (
+          <Button
+            text="확인"
+            variant="outline"
+            asChild
+            className={actionButton}
+            textSize="sm"
+            textWeight="semibold"
+          />
+        )}
+      </div>
     </div>
   );
 }
