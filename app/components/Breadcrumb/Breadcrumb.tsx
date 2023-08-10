8681d3b4ac751eab373bd51f99139e075b3b421f
Eunyoung Park <108717962+Zoey425@users.noreply.github.com>
2023-08-10 21:07:27 +0900

    Refactor:: Breadcrumb의 불필요한 코드 리팩토링 (#69)

--- app/components/Breadcrumb/Breadcrumb.tsx
+++ app/components/Breadcrumb/Breadcrumb.tsx
@@ -1,23 +1,33 @@
+import Image from "next/image";
+import Text, { TextProps } from "@/app/components/Text.tsx";
+import arrow from "@/app/assets/arrow.svg";
 import Text from "@/app/components/Text.tsx";
 import Icon from "@/app/components/Icon/Icon";
 
-interface BreadcrumbItem {
-  label: string;
-}
 
 interface BreadcrumbProps {
   menu: string[];
+  className?: string;
+  textSize?: TextProps["size"];
+  textWeight?: TextProps["weight"];
 }
 
-export default function Breadcrumb({ menu }: BreadcrumbProps) {
-  const breadcrumbs: BreadcrumbItem[] = menu.map(label => ({ label }));
-
+export default function Breadcrumb({
+  menu,
+  className,
+  textSize = "lg",
+  textWeight = "semibold",
+}: BreadcrumbProps) {
   return (
-    <nav>
+    <nav className={` ${className}`}>
       <ol className="list-none inline-flex">
-        {breadcrumbs.map((item, index) => (
-          <li key={item.label} className="flex items-center">
+        {menu.map((item, index) => (
+          <li
+            key={item}
+            className={`flex items-center ${index !== 0 ? "mx-2" : ""}`}
+          >
             {index !== 0 && (
+              <Image src={arrow} alt="breadcrumb_arrow_img" className="mr-2" />
               <Icon
                 name="BreadArrow"
                 className="mx-2 !fill-Grayscale-40"
@@ -25,8 +35,12 @@ export default function Breadcrumb({ menu }: BreadcrumbProps) {
                 height={10}
               />
             )}
-            <Text size="lg" weight="semibold" className="text-Grayscale-80">
-              {item.label}
+            <Text
+              size={textSize}
+              weight={textWeight}
+              className="text-Grayscale-80"
+            >
+              {item}
             </Text>
           </li>
         ))}
